40c6aa857635a2112850e48c8586a8b4b927102e
pyquick <fairzlq@hotmail.com>
2026-07-12 16:56:51 +0800

    test channel

--- MacBoxTool/constants.py
+++ MacBoxTool/constants.py
@@ -26,7 +26,7 @@ class Constants:
         self.launcher_script:           str = None
         # OpenCore Version
         self.opencore_version:           str = "1.0.6"
-        self.nightly_build:              str = "4535.1000"
+        self.nightly_build:              str = "4535.1100"
         self.support_version:            str = "1.0.245834435prefix" # prefix: unstable core.
 
         # Kext Versioning
