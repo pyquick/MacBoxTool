efed4a6fa72547c6d434e67d56a03d7afdaeb02e
Ghltbm <138438187+pyquick@users.noreply.github.com>
2026-07-26 19:20:40 +0800

    update Api

--- gen.py
+++ gen.py
@@ -17,46 +17,143 @@ def generate_manifest(commit_info: tuple, manifest_path: Path = DEFAULT_MANIFEST
         is_nightly = False
     else:
         is_nightly = True
-
+    
     response = requests.get(FETCH_URL,verify=False)
+    branch=str(commit_info[0]).split("/")[-1]
     manifest = response.json()
-    nightly_latest = manifest["nightly_latest"]
-    stable_latest = manifest["stable_latest"]
+    nightly_latest_on_main_branch = manifest["nightly_latest"]["main"]
+    nightly_latest_on_pyside2_branch = manifest["nightly_latest"]["PySide2"]
+    stable_latest_on_main_branch = manifest["stable_latest"]["main"]
+    stable_latest_on_pyside2_branch = manifest["stable_latest"]["PySide2"]
 
     if is_nightly:
-        version = {
-            "nightly_latest": {
-                "version":constant.macboxtool_version,
-                "build":constant.nightly_build,
-                "core":constant.support_version,
-            },
-            "stable_latest":{
-                "version":stable_latest["version"],
-                "build":stable_latest["build"],
-                "core":stable_latest["core"]
+        if branch=="main":
+            version = {
+                "nightly_latest": {
+                    "main":{
+                        "version":constant.macboxtool_version,
+                        "build":constant.nightly_build,
+                        "core":constant.support_version,
+                        "minver":"22",
+                    },
+                    "PySide2":{
+                        "version":nightly_latest_on_pyside2_branch["version"],
+                        "build":nightly_latest_on_pyside2_branch["build"],
+                        "core":nightly_latest_on_pyside2_branch["core"],
+                        "minver":"17",
+                    },
+                },
+                "stable_latest":{
+                    "main":{
+                        "version":stable_latest_on_main_branch["version"],
+                        "build":stable_latest_on_main_branch["build"],
+                        "core":stable_latest_on_main_branch["core"],
+                        "minver":"22",
+                    },
+                    "PySide2":{
+                        "version":stable_latest_on_pyside2_branch["version"],
+                        "build":stable_latest_on_pyside2_branch["build"],
+                        "core":stable_latest_on_pyside2_branch["core"],
+                        "minver":"17",
+                    },
+                }
             }
-        }
+        elif branch=="PySide2":
+            version = {
+                "nightly_latest": {
+                    "main":{
+                        "version":nightly_latest_on_main_branch["version"],
+                        "build":nightly_latest_on_main_branch["build"],
+                        "core":nightly_latest_on_main_branch["core"],
+                        "minver":"22",
+                    },
+                    "PySide2":{
+                        "version":constant.macboxtool_version,
+                        "build":constant.nightly_build,
+                        "core":constant.support_version,
+                        "minver":"17",
+                    },
+                },
+                "stable_latest":{
+                    "main":{
+                        "version":stable_latest_on_main_branch["version"],
+                        "build":stable_latest_on_main_branch["build"],
+                        "core":stable_latest_on_main_branch["core"],
+                        "minver":"22",
+                    },
+                    "PySide2":{
+                        "version":stable_latest_on_pyside2_branch["version"],
+                        "build":stable_latest_on_pyside2_branch["build"],
+                        "core":stable_latest_on_pyside2_branch["core"],
+                        "minver":"17",
+                    },
+                }
+            }
     else:
-        version = {
-            "Note":"Please update to 0.0.3.2(Nightly) by 2026-07-02-00:00:00 UTC.",
-            "version": constant.macboxtool_version,
-            "build": constant.nightly_build,
-            "core": constant.support_version,
-            "nightly":is_nightly,
-            "nightly_latest":{
-                "version":nightly_latest["version"],
-                "build":nightly_latest["build"],
-                "core":nightly_latest["core"]
-            },
-            "stable_latest": {
-                "version":constant.macboxtool_version,
-                "build":constant.nightly_build,
-                "core":constant.support_version,
+        if branch=="main":
+            version = {
+                "nightly_latest": {
+                    "main":{
+                        "version":nightly_latest_on_main_branch["version"],
+                        "build":nightly_latest_on_main_branch["build"],
+                        "core":nightly_latest_on_main_branch["core"],
+                        "minver":"22",
+                    },
+                    "PySide2":{
+                        "version":nightly_latest_on_pyside2_branch["version"],
+                        "build":nightly_latest_on_pyside2_branch["build"],
+                        "core":nightly_latest_on_pyside2_branch["core"],
+                        "minver":"17",
+                    },
+                },
+                "stable_latest":{
+                    "main":{
+                        "version":constant.macboxtool_version,
+                        "build":constant.nightly_build,
+                        "core":constant.support_version,
+                        "minver":"22",
+                    },
+                    "PySide2":{
+                        "version":stable_latest_on_pyside2_branch["version"],
+                        "build":stable_latest_on_pyside2_branch["build"],
+                        "core":stable_latest_on_pyside2_branch["core"],
+                        "minver":"17",
+                    },
+                }
             }
-        }
-
+        elif branch=="PySide2":
+            version = {
+                "nightly_latest": {
+                    "main":{
+                        "version":nightly_latest_on_main_branch["version"],
+                        "build":nightly_latest_on_main_branch["build"],
+                        "core":nightly_latest_on_main_branch["core"],
+                        "minver":"22",
+                    },
+                    
+                    "PySide2":{
+                        "version":nightly_latest_on_pyside2_branch["version"],
+                        "build":nightly_latest_on_pyside2_branch["build"],
+                        "core":nightly_latest_on_pyside2_branch["core"],
+                        "minver":"17",
+                    },
+                    
+                },
+                "stable_latest":{
+                    "main":{
+                        "version":stable_latest_on_main_branch["version"],
+                        "build":stable_latest_on_main_branch["build"],
+                        "core":stable_latest_on_main_branch["core"],
+                        "minver":"22",
+                    },
+                    "PySide2":{
+                        "version":constant.macboxtool_version,
+                        "build":constant.nightly_build,
+                        "core":constant.support_version,
+                        "minver":"17",
+                    },
+                }
+            }
+    print(version)
     with open(manifest_path, "w+", encoding="utf-8") as fs:
         json.dump(version, fs, indent=4)
-
-    print("manifest.json is created.")
-
